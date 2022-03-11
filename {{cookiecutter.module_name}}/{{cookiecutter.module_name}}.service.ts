c4afac56d89648f9199c30d8b18e61cc673dddc3
Mathis Pinsault <pinsaultm@gmail.com>
2022-03-11 04:37:14 +0100

    First commit

--- {{cookiecutter.module_name}}/{{cookiecutter.module_name}}.service.ts
+++ {{cookiecutter.module_name}}/{{cookiecutter.module_name}}.service.ts
@@ -3,11 +3,4 @@ import {{ cookiecutter.Module_Name }} from "src/types/{{cookiecutter.Module_Name
 
 @Injectable()
 export class {{ cookiecutter.Module_Name }}Service {
-  {% if cookiecutter.module_routes | length > 0 %}
-  {% for item in cookiecutter.module_routes | list %}
-  {{ item.name }} (): string {
-    return "{{item.name}}";
-  }
-  {% endfor %}
-  {% endif %}
 }
