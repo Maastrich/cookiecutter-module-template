6c7e668c5326382c23f602ac6a3875a93bf3d995
Mathis Pinsault <pinsaultm@gmail.com>
2022-03-11 05:14:19 +0100

    commit

--- {{cookiecutter.module_name}}/{{cookiecutter.module_name}}.controller.ts
+++ {{cookiecutter.module_name}}/{{cookiecutter.module_name}}.controller.ts
@@ -5,9 +5,8 @@ import { {{ cookiecutter.module_mame_pascal }}Service } from "./{{cookiecutter.m
 constructor(private readonly {{ cookiecutter.module_name }}Service: {{ cookiecutter.module_mame_pascal }}Service) { }
 export class {{ cookiecutter.module_mame_pascal }}Controller {
 
-  {% for key, item in cookiecutter.module_routes.items() %}
+  {% for key in cookiecutter.module_routes %}
   @Get("{{ key }}")
-  {{ item }}
   {% endfor %}
 
 }
