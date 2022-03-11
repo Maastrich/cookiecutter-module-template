8cb9c9b8fd9ff7934f7492b51b779885d3acce1c
Mathis Pinsault <pinsaultm@gmail.com>
2022-03-11 04:24:30 +0100

    First commit

--- {{cookiecutter.module_name}}/{{cookiecutter.module_name}}.controller.ts
+++ {{cookiecutter.module_name}}/{{cookiecutter.module_name}}.controller.ts
@@ -1,16 +1,17 @@
-import { Controller, Get, Put, Delete, Post } from '@nestjs/common';
-import {{{cookiecutter.Module_Name}}Service } from './{{cookiecutter.module_name}}.service';
+import { Controller, Get, Put, Delete, Post } from "@nestjs/common";
+import {{{ cookiecutter.Module_Name }}Service } from "./{{cookiecutter.module_name}}.service";
 
-@Controller('{{cookiecutter.module_path}}')
-export class {{cookiecutter.Module_Name}}Controller {
-  constructor(private readonly {{cookiecutter.module_name}}Service: { { cookiecutter.Module_Name } }Service) { }
+@Controller("{{cookiecutter.module_path}}")
+export class {{ cookiecutter.Module_Name }}Controller {
+  constructor(private readonly {{ cookiecutter.module_name }} Service: { { cookiecutter.Module_Name } }Service) { }
 
-  {% if cookiecutter.module_routes|length > 0 %}
-  {% for item in cookiecutter.module_routes %}
-  @{{item.method}}('{{item.path}}')
-  {{item.name}}(): string {
-    return {{cookiecutter.module_name}}Service.{{item.name}}();
+{% if cookiecutter.module_routes | length > 0 %}
+{% for item in cookiecutter.module_routes %}
+@{{ item.method }}("{{item.path}}")
+{ { item.name } } (): string {
+  return {{ cookiecutter.module_name }
+}Service.{ { item.name } } ();
   }
-  {% endfor %}
-  {% endif %}
+{% endfor %}
+{% endif %}
 }
--- {{cookiecutter.module_name}}/{{cookiecutter.module_name}}.service.ts
+++ {{cookiecutter.module_name}}/{{cookiecutter.module_name}}.service.ts
@@ -1,12 +1,12 @@
-import { Injectable } from '@nestjs/common';
-import {{ cookiecutter.Module_Name }} from 'src/types/{{cookiecutter.Module_Name}}';
+import { Injectable } from "@nestjs/common";
+import {{ cookiecutter.Module_Name }} from "src/types/{{cookiecutter.Module_Name}}";
 
 @Injectable()
 export class {{ cookiecutter.Module_Name }}Service {
-  {% if cookiecutter.module_routes|length > 0 %}
-  {% for item in cookiecutter.module_routes|list %}
-  {{item.name}}(): string {
-    return '{{item.name}}';
+  {% if cookiecutter.module_routes | length > 0 %}
+  {% for item in cookiecutter.module_routes | list %}
+  { { item.name } } (): string {
+    return "{{item.name}}";
   }
   {% endfor %}
   {% endif %}
